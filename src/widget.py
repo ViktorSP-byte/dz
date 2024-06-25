1ca0d989702622160f60b4c051aaddc9ddb4732e
Viktor <flystrike1994@gmail.com>
2024-06-25 13:18:31 +0300

    file done

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
 def mask_account_card(card: str):
+    """Функция, маскирующая номер карты и счета"""
     for member in card:
         if "V" in member:
             mask = card.replace(card[14:-4], "******")
@@ -13,7 +14,18 @@ def mask_account_card(card: str):
             result = mask
             return result
 
+
 card = "Maestro 1596837868705199"
 
 
 print(mask_account_card(card))
+
+
+def get_data(data):
+    """Функция, меняющая формат даты"""
+    result = data[8:10] + "." + data[5:7] + "." + data[:4]
+    return result
+
+
+data = "2018-07-11T02:26:18.671407"
+print(get_data(data))
