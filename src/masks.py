f91fb8aa5e07f838fe9c1d9426472b6836e8a03b
Viktor <flystrike1994@gmail.com>
2024-06-29 15:05:07 +0300

    ntw

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,14 @@
-def mask_account_card(card: str):
-    """Функция, маскирующая номер карты и счета"""
-    for _card in card:
-        if "Счет" in card:
-            mask_1 = card.replace(x[5:-4], "**")
-            result = mask_1
-            return result
-        else:
-            mask_2 = card.replace(x[14:-4], "******")
-            result = mask_2
-            return result
+def get_mask_card_number(card_2: str):
+    """Функция, маскирующая номер карты"""
+    for _card in card_2:
+        mask_1 = card_2.replace(card_2[6:-4], "******")
+        result = mask_1
+        return result
 
 
-x = "Счет 64686473678894779589"
-print(mask_account_card(x))
+def get_mask_account(card_1: str):
+    """"Функция, маскирующая номер счета"""
+    for _card in card_1:
+        mask_2 = card_1.replace(card_1[:-4], '**')
+        result = mask_2
+        return result
--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,16 @@
-import masks
+from src.masks import get_mask_account, get_mask_card_number
+def mask_account_card(account_info: str) -> str:
+    """Функция, возвращающая маску карты или счета"""
+    if account_info[:4] == "Счет":
+        account_number = get_mask_account(account_info[5:])
+        mask_account = f"{account_info[:4]} {account_number}"
+        return mask_account
+    else:
+        mask_card_number = get_mask_card_number(account_info[-16:])
+        mask_card = f"{account_info[:-16]} {mask_card_number}"
+        return mask_card
 
+print(mask_account_card("Maestro 1596837868705199"))
 def get_data(data):
     """Функция, меняющая формат даты"""
     result = data[8:10] + "." + data[5:7] + "." + data[:4]
