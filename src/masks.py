9182d0d49e9825c476452837287f94f762615729
Viktor <flystrike1994@gmail.com>
2024-07-01 13:40:01 +0300

    develop

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,20 @@
-def get_mask_card_number(card_2: str):
+def get_mask_card_number(card_2: str) -> str:
     """Функция, маскирующая номер карты"""
     for _card in card_2:
         mask_1 = card_2.replace(card_2[6:-4], "******")
         result = mask_1
-        return result
+    return result
 
 
-def get_mask_account(card_1: str):
+card_2 = "7000792289606361"
+
+
+def get_mask_account(card_1: str) -> str:
     """"Функция, маскирующая номер счета"""
     for _card in card_1:
         mask_2 = card_1.replace(card_1[:-4], '**')
         result = mask_2
-        return result
+    return result
+
+
+card_1 = "73654108430135874305"
